05eda568c1e745198e64796bea62573dcc159133
sshesharaj <82090385+sshesharaj@users.noreply.github.com>
2021-06-24 15:20:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# c20p
-class 20 project
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,124 @@
+var car , wall , thickness; 
+ var speed , weight;
+ 
+ 
+ function setup() {
+  createCanvas(1600,400);
+   car = createSprite(50, 200, 50, 50);
+   
+
+speed = random(223 , 321);
+weight = random (30 , 52);
+thickness = random(22,83);
+
+wall = createSprite(1200,200,thickness, height/2)
+
+
+car.velocityX = speed ;
+
+
+
+
+
+
+}
+
+function draw() {
+  background(255,255,255); 
+
+if (hasCollided(car,wall)){
+  car.velocityX = 0;
+  var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+
+
+  if (damage>10){
+    wall.shapeColor = color(255,0,0)
+  }
+
+  if (damage<10){
+    wall.shapeColor = color(0,255,0)
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
+  
+  
+if (wall.x - car.x < (car.width + width)/2){
+  car.velocityX = 0 ;
+  var deformation = 0.5 * weight * speed * speed/22500 ; 
+
+
+  if (deformation > 180){
+
+    car.shapeColor = "red";
+  }
+
+  if (deformation < 180 && deformation > 100){
+
+    car.shapeColor = "yellow";
+
+  }
+
+  if (deformation < 100){
+
+    car.shapeColor = "green"
+
+  }
+
+
+
+
+
+}
+
+
+
+
+
+
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+
+  bulletRightEdge = lbullet.x + lbullet.width;
+wallLeftEdge = lwall.x
+
+
+
+if(bulletRightEdge>= wallLeftEdge){
+
+return true;
+}
+else {
+  return false;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
